5e00237ad2a77885fca803e1fde29f0a9802ba8b
Terabaappudina <133086968+Terabaappudina@users.noreply.github.com>
2023-06-18 08:57:00 +0530

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -1,20 +1,16 @@
-#Github.com/mrinvisible7
-
 import os
 from .. import bot as Invix
 from telethon import events, Button
 
-#from ethon.mystarts import start_srb
-    
 S = '/' + 's' + 't' + 'a' + 'r' + 't'
 
 @Invix.on(events.callbackquery.CallbackQuery(data="set"))
-async def sett(event):    
+async def sett(event):
     Invix = event.client
     button = await event.get_message()
     msg = await button.get_reply_message()
     await event.delete()
-    async with Invix.conversation(event.chat_id) as conv: 
+    async with Invix.conversation(event.chat_id) as conv:
         xx = await conv.send_message("Send me any image for thumbnail as a `reply` to this message.")
         x = await conv.get_reply()
         if not x.media:
@@ -29,32 +25,24 @@ async def sett(event):
             os.remove(f'{event.sender_id}.jpg')
         os.rename(path, f'./{event.sender_id}.jpg')
         await t.edit("Temporary thumbnail saved!")
-        
+
 @Invix.on(events.callbackquery.CallbackQuery(data="rem"))
-async def remt(event):  
-    Invix = event.client            
+async def remt(event):
+    Invix = event.client
     await event.edit('Trying.')
     try:
         os.remove(f'{event.sender_id}.jpg')
         await event.edit('Removed!')
     except Exception:
-        await event.edit("No thumbnail saved.")                        
-  
+        await event.edit("No thumbnail saved.")
+
 @Invix.on(events.NewMessage(incoming=True, pattern=f"{S}"))
 async def start(event):
-    text = "👋 Hi, I am 'Save Restricted Content ' bot.\n\n✅ Send me the Link of any message of Restricted Channels to Clone it here. 
-For private channel's messages, send the Invite Link first.\n\n👨🏻‍💻Owner: @mister_invisiblebot.\n**support:** @mr_invisible_bots"
-    #await start_srb(event, text)
-    await event.reply(text, 
-                      buttons=[
-                              [Button.inline("SET THUMB.", data="set"),
-                               Button.inline("REM THUMB.", data="rem")]
-                              ])
-    '''
-    await event.reply(text, 
-                      buttons=[
-                              [Button.inline("SET THUMB.", data="set"),
-                               Button.inline("REM THUMB.", data="rem")],
-                              [Button.url("Maintained and Modified by", url="t.me/mr_invisible_bots")]])
-    '''
+    text = "👋 Hi, I am 'Save Restricted Content' bot.\n\n✅ Send me the Link of any message of Restricted Channels to Clone it here. For private channel's messages, send the Invite Link first.\n\n👨🏻‍💻 Owner: @mister_invisiblebot.\n**support:** @mr_invisible_bots"
+    await event.reply(text, buttons=[
+        [Button.inline("SET THUMB.", data="set"),
+         Button.inline("REM THUMB.", data="rem")]
+    ])
+
+
     
