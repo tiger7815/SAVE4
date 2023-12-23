48efa66b3051d31cfda449ce4a5a7c809f9cf83a
TIGER <141488144+tiger7815@users.noreply.github.com>
2023-12-23 06:00:30 +0530

    Update __main__.py

--- main/__main__.py
+++ main/__main__.py
@@ -17,7 +17,7 @@ botStartTime = time.time()
 
 
 print("Successfully deployed!")
-print("By Mr. Invisible • Mr_Invisible_bots")
+print("By Mr. TIGER • Mr_TIGER_bots")
 
 if __name__ == "__main__":
     from . import bot
